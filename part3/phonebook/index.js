e30d721f6ec5d5cf5a41468f89cb5697dfa368fc
Risto Koverola <risto.koverola@eficode.com>
2022-06-15 11:53:55 +0300

    Solve 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('custom', function (req, res) { return JSON.stringify(req.body) })
 
 const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :custom'))
+app.use(cors())
 
 let persons = [
     { 
