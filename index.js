4ab635090977a3ea42c1dc6e89676235632aef7e
Risto Koverola <risto.koverola@eficode.com>
2022-06-15 13:31:33 +0300

    uibuild

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :custom'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { 
