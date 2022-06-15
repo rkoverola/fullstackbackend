64534b2cd92cca4d6fa3f194e81be391b7673e1a
Risto Koverola <risto.koverola@eficode.com>
2022-06-15 12:07:17 +0300

    Prepare backend for deployment

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -83,7 +83,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Phonebook server running on port ${PORT}`)
 })
